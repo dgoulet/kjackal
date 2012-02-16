5f7a6d323dbb281fd4390e6ccd3a834d48e70476
David Goulet <dgoulet@ev0ke.net>
2012-02-15 22:35:11 -0500

    Fix kernel symbol lookup
    To use module_kset and sys_call_table kernel symbol, using
    kallsyms_lookup_name, the CONFIG_KALLSYMS_ALL is needed which is not
    standard on today's distros.
    
    Now, the Makefile get the address value from the System-map-* file and
    sed them into the src/common.c source file.
    
    Adds the check for CONFIG_KALLSYMS_ALL.
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- Makefile
+++ Makefile
@@ -1,8 +1,24 @@
 obj-m += kjackal.o
 kjackal-objs := src/common.o src/module.o src/syscall.o src/tcp4.o src/proc_fs.o src/init.o
 
+MOD_KSET=$(shell grep ' module_kset' /boot/System.map-$(shell uname -r) | awk '{print $$1}')
+TABLE=$(shell grep ' sys_call_table' /boot/System.map-$(shell uname -r) | awk '{print $$1}')
+KERN_TEXT=$(shell grep ' core_kernel_text' /boot/System.map-$(shell uname -r) | awk '{print $$1}')
+
 all:
+	@echo "Replacing sys call table address $(TABLE) in source"
+	@echo "Replacing module_kset address $(MOD_KSET) in source"
+	@echo "Replacing core_kernel_text address $(KERN_TEXT) in source"
+	@echo
+	@sed -i 's/SYS_CALL_TABLE/$(TABLE)/g' src/common.c
+	@sed -i 's/MODULE_KSET/$(MOD_KSET)/g' src/common.c
+	@sed -i 's/CORE_KERNEL_TEXT/$(KERN_TEXT)/g' src/common.c
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	@echo
+	# Restoring symbol in source code
+	@sed -i 's/$(TABLE)/SYS_CALL_TABLE/g' src/common.c
+	@sed -i 's/$(MOD_KSET)/MODULE_KSET/g' src/common.c
+	@sed -i 's/$(KERN_TEXT)/CORE_KERNEL_TEXT/g' src/common.c
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 
--- src/common.c
+++ src/common.c
@@ -25,7 +25,11 @@
 
 static int (*__kernel_text_address_sym)(unsigned long addr);
 
-#ifdef CONFIG_KALLSYMS
+/*
+ * Two out of three symbols needed by kjackal needs CONFIG_KALLSYMS_ALL. The
+ * fallback is to use System-map-* with the Makefile.
+ */
+#if defined(CONFIG_KALLSYMS) && defined(CONFIG_KALLSYMS_ALL)
 
 /*
  * kallsyms_lookup_name was re-exported in from 2.6.33 +
@@ -36,10 +40,35 @@ void *lookup_kernel_symbol(const char *name)
 	return (void *) kallsyms_lookup_name(name);
 }
 #else
+/*
+ * kallsyms is available but the lookup function is not exported before this
+ * kernel version so we simply stop compilation.
+ */
 #error "Kernel version not compatible (version >= 2.6.33)"
 #endif /* LINUX_VERSION_CODE */
 
-#endif /* CONFIG_KALLSYMS */
+#else /* CONFIG_KALLSYMS && CONFIG_KALLSYMS_ALL */
+void *lookup_kernel_symbol(const char *name)
+{
+	void *sym = NULL;
+
+	/*
+	 * SYS_CALL_TABLE, MODULE_KSET AND CORE_KERNEL_TEXT are replaced by the
+	 * Makefile. Those addresses are red from the System-map-$(uname -r) file.
+	 *
+	 * DO NOT CHANGE this arbitrarily unles you know what you are doing.
+	 */
+	if (strcmp(name, "sys_call_table") == 0) {
+		sym = (void*)0xSYS_CALL_TABLE;
+	} else if (strcmp(name, "module_kset") == 0) {
+		sym = (void*)0xMODULE_KSET;
+	} else if (strcmp(name, "core_kernel_text") == 0) {
+		sym = (void*)0xCORE_KERNEL_TEXT;
+	}
+
+	return sym;
+}
+#endif /* CONFIG_KALLSYMS && CONFIG_KALLSYMS_ALL */
 
 /*
  * Check if addr is in core kernel text.
